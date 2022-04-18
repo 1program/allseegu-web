6f19dfc01e685870d62f40e3578a24e847698542
tagpainter <services.tagpainter@gmail.com>
2022-04-18 12:37:41 +0900

    우리구역이야기 더보기 버튼 노출 처리 (없을 경우 작성이 가능해야 한다)

--- src/views/redev/[redev_id]/components/StorySection.vue
+++ src/views/redev/[redev_id]/components/StorySection.vue
@@ -14,9 +14,7 @@
       </template>
     </div>
   </div>
-  <MoreButton v-if="stories.length > 0" :to="`/redev/${redev_id}/story`">
-    우리 구역 이야기 더보기
-  </MoreButton>
+  <MoreButton :to="`/redev/${redev_id}/story`">우리 구역 이야기 더보기 </MoreButton>
 </template>
 
 <script lang="ts">
